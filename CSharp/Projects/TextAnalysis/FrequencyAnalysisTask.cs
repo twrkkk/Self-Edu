49f379a1d154dcae0b44378305005f2c90870dd7
twrkkk <zhenya.shein.03@gmail.com>
2023-03-13 12:13:25 +0300

    Continue phrase (Ngramm Viewer)

---
+++ CSharp/Projects/TextAnalysis/FrequencyAnalysisTask.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TextAnalysis
+{
+    static class FrequencyAnalysisTask
+    {
+        private static Dictionary<string, Dictionary<string, int>> GetAllNGramms(List<List<string>> text, int n)
+        {
+            Dictionary<string, Dictionary<string, int>> result = new Dictionary<string, Dictionary<string, int>>();
+
+            foreach (var sent in text)
+            {
+                for (int i = 0; i < sent.Count - n + 1; i++)
+                {
+                    var startNGram = string.Join(" ", sent.GetRange(i, n - 1));
+                    var endNGram = sent[i + n - 1];
+
+                    if (!result.ContainsKey(startNGram))
+                    {
+                        var d = new Dictionary<string, int>();
+                        d.Add(endNGram, 1);
+                        result.Add(startNGram, d);
+                    }
+                    else
+                    {
+                        if (!result[startNGram].ContainsKey(endNGram))
+                            result[startNGram].Add(endNGram, 1);
+                        else
+                            result[startNGram][endNGram]++;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static Dictionary<string, string> GetMostFrequentNGramm(Dictionary<string, Dictionary<string, int>> nGramms)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            foreach (var phrase in nGramms)
+            {
+                int maxLen = 0;
+                string mostFreqStr = null;
+                foreach (var ending in phrase.Value)
+                {
+                    if (ending.Value > maxLen)
+                    {
+                        mostFreqStr = ending.Key;
+                        maxLen = ending.Value;
+                    }
+                    else if (ending.Value == maxLen && string.CompareOrdinal(ending.Key, mostFreqStr) < 0)
+                    {
+                        mostFreqStr = ending.Key;
+                        maxLen = ending.Value;
+                    }
+
+                }
+
+                result.Add(phrase.Key, mostFreqStr);
+            }
+
+            return result;
+        }
+        public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
+        {
+            return GetMostFrequentNGramm(GetAllNGramms(text, 2)
+                                        .Concat(GetAllNGramms(text, 3))
+                                        .ToDictionary(k => k.Key, v => v.Value));
+        }
+    }
+}
---
+++ CSharp/Projects/TextAnalysis/SentencesParserTask.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace TextAnalysis
+{
+    static class SentencesParserTask
+    {
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+            char[] delimiters = { '.', '!', '?', ';', ':', '(', ')' };
+            foreach (var sentence in text.Split(delimiters))
+            {
+                if (sentence.Length > 0)
+                {
+                    List<string> ListOfWords = DevideByWords(sentence);
+                    if (ListOfWords != null && ListOfWords.Count > 0)
+                        sentencesList.Add(ListOfWords);
+                }
+            }
+            return sentencesList;
+        }
+
+        private static List<string> DevideByWords(string sentence)
+        {
+            if (string.IsNullOrWhiteSpace(sentence)) return null;
+            List<string> result = new List<string>();
+            var words = sentence.Split(' ');//new char[] { ' ', ',', '\n', '\r', '\t' });
+            foreach (var word in words)
+            {
+                foreach (var item in RemoveSeparators(word).Split(' '))
+                {
+                    if (!string.IsNullOrEmpty(item))
+                        result.Add(item.ToLower());
+                }
+
+            }
+            return result;
+        }
+
+        private static string RemoveSeparators(string word)
+        {
+            int length = word.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if (!char.IsLetter(word[i]) && word[i] != '\'')
+                    word = word.Replace(word[i], ' ');
+            }
+            return word;
+        }
+
+    }
+}
